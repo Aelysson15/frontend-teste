d1530fc004665a0c8ec9feb647a87a7f1b8a4edb
aelysson <jose.aelysson@unifacisa.edu.br>
2020-10-03 11:19:51 -0300

    ajustes

--- index.php
+++ index.php
@@ -15,62 +15,58 @@
 
 </head>
 <body>
-<!-- header -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img class="logo" src="assets/imgs/logo.png" alt="Logo"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input id="email-topo" class="form-control mr-sm-2" type="search" placeholder="Your Email"
-        aria-label="Your Email">
-      <button id="btn-topo" class="btn btn-outline my-2 my-sm-0" type="submit">Subscribe</button>
-    </form>
-  </div>
-</nav>
-<!-- final header -->
-
-<!-- home -->
 <div id="banner" class="jumbotron jumbotron-fluid">
+  <!-- header -->
+  <nav id="navegador" class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#"><img class="logo" src="assets/imgs/logo.png" alt="Logo"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input id="email-topo" class="form-control mr-sm-2" type="search" placeholder="Your Email"
+          aria-label="Your Email">
+          <button id="btn-topo" class="btn btn-outline" type="submit">Subscribe</button>
+      </form>
+    </div>
+  </nav>
+  <!-- baner -->
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-6">
-        <img class="img-fluid" src="assets/imgs/design-img.jpg" alt="">
+        <img class="img-fluid tela" src="assets/imgs/tela.png" alt="Tela">
       </div>
-      <div class="col-md-6">
-        <h1 class="display-4">We focus on ergonomics</h1>
-        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      <div class="offset-md-1 col-md-5">
+        <h1 class="display-4 titulo-banner">We focus on ergonomics</h1>
+        <p class="minetext">Most calendars are designed for teams. <br>
+          Slate is designed for freelancers</p><br>
+          <button type="button" class="btn btn-primary btn-try-bg">Try For Free</button>
       </div>
     </div>
   </div>
 </div>
 
+<!-- home -->
 <section id="features">
   <img class="img-fluid onda" src="assets/imgs/onda_banner.png" alt="">
   <div id="bg" class="container-fluid text-center">
@@ -115,7 +111,7 @@
     <button type="button" class="btn btn-primary btn-try">Try For Free</button>
 
     <div id="img-proto" class="container">
-      <img class="img-responsive" src="assets/imgs/proto.png" alt="">
+      <img class="img-fluid" src="assets/imgs/proto.png" alt="">
     </div>
   </div>
 </section>
